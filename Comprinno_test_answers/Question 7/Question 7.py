4eda4e3244d3909fde6f5f9e606cdd3d21cd48ae
Shubashkaran <63039642+Shubashkaran@users.noreply.github.com>
2021-05-26 17:16:26 +0530

    Update Question 7.py

--- Comprinno_test_answers/Question 7/Question 7.py
+++ Comprinno_test_answers/Question 7/Question 7.py
@@ -1,19 +1,18 @@
 for T in range(int(input())):
-    N=int(input())
-    A=list(map(int,input().split()))
-    A.sort()
-    res=1
-    c=0
-    i=N-1
-    while i>=0:
-        if A[i]==A[i-1]:
-            c=c+1
-            res=res*A[i]
-            i=i-1
-        if c==2:
-            break
-        i=i-1
-    if c==2:
-        print(res)
+    N=input()
+    L=list(map(int,input().split()))
+    L2=[]
+    for i in range(len(L)):
+        #appending list values whose count in list L is high, so that we can use the sticks with higher length
+        if L.count(L[i])>1:
+            L2.append(L[i])
+    #checking if there are sticks sufficient number of sticks to build a rectangle 
+    if len(l2)>3:
+        #Sorting and Reversing so that higher length sticks can we used first
+        l2.sort()
+        l2.reverse()
+        #Finding the maximum possible area of the constructed rectangle
+        print(l2[0]*l2[3])
     else:
-        print("-1")
+        #Otherwise, it is not possible to build rectangle with given no of higher length sticks
+        print('-1')
